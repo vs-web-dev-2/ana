6ef3a23b90c9ce10874e47fe2db388d6d0f62176
anauzubi <ana.uribe@softtek.com>
2021-06-09 16:17:22 +0200

    funcion cal importe

--- ivaImporte.js
+++ ivaImporte.js
@@ -1,12 +1,17 @@
 function calcularIva(importe, impuesto=21){
-    return importe * impuesto / 100;
+    return importe * impuesto /100;
 }
+console.log(calcularIva(10));
 
-function calcularImporteTotal(importe,impuesto){
-    return importe + calcularIva(importe,impuesto);
+function calcularImporte(importe, iva){
+    return importe + iva
 }
+let importe = 10;
+let iva = calcularIva(importe, impuesto=21);
+let importeTotal =calcularImporte(importe, iva);
 
-let importeBase = 100;
-let importeTotal = calcularImporteTotal(importeBase);
-// console.log("El importe total de " + importeBase + " es " + importeTotal);
-console.log(`El importe total de ${importeBase} es ${importeTotal}`);
+console.log("El iva de  " + importe + " es " + iva + " y el importe total es "+ importeTotal);
+console.log(
+    `El IVA de ${importe} es ${iva}
+        y el total es  ${importeTotal}`
+  );
